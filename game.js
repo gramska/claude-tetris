34b7243426be5fa90d3b99a034f0428b6a425049
gabo <gabinoramos@gmail.com>
2026-09-03 21:02:21 -0600

    Fix: parar elbucle de piezas en game over

--- game.js
+++ game.js
@@ -214,6 +214,8 @@ function draw() {
     for (let c = 0; c < COLS; c++)
       drawBlock(ctx, c, r, board[r][c], BLOCK);
 
+  if (gameOver) return;
+
   // ghost
   const gy = ghostY();
   for (let r = 0; r < current.shape.length; r++)
@@ -273,6 +275,7 @@ function loop(ts) {
     }
   }
   draw();
+  if (gameOver || paused) return;
   animId = requestAnimationFrame(loop);
 }
 
